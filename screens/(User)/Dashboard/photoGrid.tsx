7dbcd779bf9f92b49983bba6b34c9b6294e8347f
franklin <frankagu92@gmail.com>
2024-04-08 16:19:55 +0100

    centered photo grid

--- screens/(User)/Dashboard/photoGrid.tsx
+++ screens/(User)/Dashboard/photoGrid.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 const PhotoGrid = () => {
   return (
-    <div className='custom-grid max-w-[476px] h-fit gap-2 pt-4'>
+    <div className='custom-grid max-w-[476px] h-fit gap-2 pt-4 photogrid-mq'>
         <Image src="/images/image1.svg" className='rounded-md' height={214} width={225} alt='photo'/>
-        <Image src="/images/image1.svg" className='rounded-md' height={214} width={225} alt='photo'/>
-        <Image src="/images/image1.svg" className='rounded-md' height={214} width={225} alt='photo'/>
-        <Image src="/images/image1.svg" className='rounded-md' height={214} width={225} alt='photo'/>
+        <Image src="/images/image2.svg" className='rounded-md' height={214} width={225} alt='photo'/>
+        <Image src="/images/image3.svg" className='rounded-md' height={214} width={225} alt='photo'/>
+        <Image src="/images/image4.svg" className='rounded-md' height={214} width={225} alt='photo'/>
     </div>
   )
 }
